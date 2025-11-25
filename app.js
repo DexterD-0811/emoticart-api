50c089522ffdd63348b2a22b3b9db6afdb35cfb2
dexterdocallas <ddocallas1108@gmail.com>
2025-11-26 01:56:25 +0800

    feat: try fix

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ try {
   process.exit(1);
 }
 
-const whitelist = ['http://localhost:5173', 'https://emoticart-admin.vercel.app'];
+const whitelist = ['http://localhost:5173', 'https://emoticart-admin-webapp.vercel.app'];
 
 app.use(
   cors({
@@ -40,6 +40,7 @@ app.use(
   })
 );
 
+app.options('*', cors({ origin: whitelist }));
 
 app.use(bodyParser.json());
 
